6b42cc3d34006001f532b2b908d7740e6694d415
ShamzAfo01 <samsudeenafolabi@gmail.com>
2026-06-24 19:27:17 +0100

    feat: add hero and banner image assets with associated product data to layout configuration

--- src/data/layout.ts
+++ src/data/layout.ts
@@ -9,3 +9,18 @@ export const getDefaultCustomLayout = (): CustomLayout => {
       name: `${p.name}`,
     }));
   };
+
+  return {
+    heroBanner: '/models/mixed-model.jpg',
+    banner1: '/models/photo_2026-06-15_15-26-05.jpg',
+    banner2: '/models/photo_2026-06-15_15-26-10.jpg',
+    banner3: '/models/photo_2026-06-15_15-26-19.jpg',
+    banner4: '/models/photo_2026-06-15_15-26-23.jpg',
+    
+    heroProducts: cloneProducts('hero'),
+    banner1Products: cloneProducts('banner1'),
+    banner2Products: cloneProducts('banner2'),
+    banner3Products: cloneProducts('banner3'),
+    banner4Products: cloneProducts('banner4'),
+  };
+};
